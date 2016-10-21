8902a7228ad35cef911b35fdf8e3242b67c1348a
joaopaulojpos <jpo.oliveiras@gmail.com>
2016-10-21 20:21:47 -0300

    Disciplina Funcionando
    Arquivo feito em sala por Matozo

--- w3/Disciplina.php
+++ w3/Disciplina.php
@@ -4,21 +4,70 @@ require_once "Conexao.php";
 $nome = $_POST['Nomedis'];
 $professor = $_POST['Professor'];
 
+
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname) 
+$conn = mysqli_connect($servername, $username, $password, $dbname); 
 //Checa a Conexao
 if (!$conn) {
 	die ("Erro ao conectar com o Banco de dados " . mysqli_connect_error());
 }
 
-$sql = "INSERT INTO disciplina (nome, professor)
-VALUES ($nome, $professor)";
+echo cadastrar_disciplina($nome , $professor);
 
-if (mysqli_query($conn, $sql)) {
-    echo "Disciplina Cadastrada com Sucesso";
-} else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
 
 mysqli_close($conn);
-?> 
+
+
+
+
+// FUNÇÔES 
+
+function validar_disciplina($professor){
+
+	global $conn;	
+
+	$sql = "Select count(*) as total from  disciplina where professor = '$professor' ";
+	$result  = mysqli_query($conn , $sql);	
+
+	$valores = mysqli_fetch_assoc($result);	
+
+	$retorno = false;	
+	if($valores['total'] > 0 ){
+		$retorno =  true;
+	}	
+
+	return $retorno ;
+
+
+
+}
+
+
+function cadastrar_disciplina($nome , $professor){
+
+	global $conn;	
+
+	$msg = "";	
+	if(! validar_disciplina($professor)){	
+		$sql = "INSERT INTO disciplina (nome, professor) VALUES ('$nome', '$professor')";
+
+		if(mysqli_query($conn , $sql)){
+
+				$msg = "Disciplina cadastrada com sucesso";
+
+		}else{
+
+				$msg ="Error" . $sql . "<br/>". mysql_error($conn);
+		}
+	}else{
+
+		$msg = "professor já existe";
+	}
+
+	return $msg;	
+}
+
+
+
+
+?>
