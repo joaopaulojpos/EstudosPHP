b5ae3bedb8ca6d885d3465d9cb19d0fb3252d7a4
joaopaulojpos <jpo.oliveiras@gmail.com>
2016-10-24 23:12:52 -0300

    Listar Disciplina funcionando

--- w3/ListarDisciplinas.php
+++ w3/ListarDisciplinas.php
@@ -1,27 +1,24 @@
 <?php
 require_once"Conexao.php";
 
-//Cria a conexao
-
+// Criando a Conexao
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-
-//Checa a Conexao
-if(!$conn){
-	die("Conexao Falhou " . mysqli_connect_error());
+// Checando a Conexao
+if (!$conn) {
+    die("Coexao Falhou: " . mysqli_connect_error());
 }
 
 $sql = "SELECT nome, professor FROM disciplina";
 $result = mysqli_query($conn, $sql);
 
-if (mysqli_num_rows($result) > 0){
-	while($row = mysqli_fetch_assoc($result))
-	{
-		echo "Nome da Disciplina: " . $row["nome"]. " - Nome do Professor: " . $row["professor"] . "<br>";
-	}
-} else{
-	echo "Nenhuma Disciplina Cadastrada";
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "Nome da Disciplina: " . $row["nome"]. " - Nome do Professor: " . $row["professor"]. "<br>";
+    }
+} else {
+    echo "0 results";
 }
 
 mysqli_close($conn);
-
 ?>
