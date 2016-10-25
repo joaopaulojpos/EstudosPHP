9849c1a32b588f9a20e60d2acc020da36afd3998
joaopaulojpos <jpo.oliveiras@gmail.com>
2016-10-24 23:25:41 -0300

    Listar disciplina completo
    Listar disciplina com o utf-8 funcionando.

--- w3/ListarDisciplinas.php
+++ w3/ListarDisciplinas.php
@@ -1,8 +1,10 @@
 <?php
+ini_set('default_charset', 'UTF-8');
 require_once"Conexao.php";
 
 // Criando a Conexao
 $conn = mysqli_connect($servername, $username, $password, $dbname);
+mysqli_set_charset($conn,"utf8"); // Acertando a acentuação
 // Checando a Conexao
 if (!$conn) {
     die("Coexao Falhou: " . mysqli_connect_error());
